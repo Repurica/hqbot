8cab2f6a16e6c709c4786bd84816fa92a7c4fb80
Repurica <65117070+Repurica@users.noreply.github.com>
2025-08-28 18:49:28 +0800

    test

--- functions/chat.py
+++ functions/chat.py
@@ -1,13 +1,22 @@
 import requests
+import random
 class Chat:
     def __init__(self,telegram_api_url):
         self.messages = []
         self.telegram_api_url = telegram_api_url
+        self.in_chat_users = {}
+        self.icons = [
+            "🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐨", "🐯", "🦁", "🐮", "🐷", "🐸", "🐵", "🐔", "🐧", "🐦", "🐤", "🦆",
+            "🦅", "🦉", "🦇", "🐺", "🐗", "🐴", "🦄", "🐝", "🐛", "🦋", "🐌", "🐞", "🐜", "🦟", "🦗", "🕷️", "🦂", "🐢", "🐍", "🦎",
+            "🦖", "🦕", "🐙", "🦑", "🦐", "🦞", "🦀", "🐡", "🐠", "🐟", "🐬", "🐳", "🐋", "🦈", "🐊", "🐅", "🐆", "🦓", "🦍", "🦧",
+            "🐘", "🦛", "🦏", "🐪", "🐫", "🦒", "🦘", "🐃", "🐂", "🐄", "🐎", "🐖", "🐏", "🐑", "🦙", "🐐", "🦌", "🐕", "🐩", "🦮",
+            "🐕‍🦺", "🐈", "🐈‍⬛", "🐓", "🦃", "🦚", "🦜", "🦢", "🦩", "🕊️", "🐇", "🦝", "🦨", "🦡", "🦦", "🦥", "🐁", "🐀", "🐿️",
+            "🦔", "🐾"
+        ]
 
     def add_message(self, user, message):
         self.messages.append({'user': user, 'message': message})
         
-
     def get_messages(self):
         return self.messages
 
@@ -24,3 +33,62 @@ class Chat:
         if parse_mode:
             payload["parse_mode"] = parse_mode
         requests.post(url, json=payload)
+        
+    async def in_chat(self, chat_id, text) -> None:
+        text = "Currently in chat:\n\n"
+        for username, (cid, icon) in self.in_chat_users.items():
+            text += f"@{username} {icon}\n"
+
+        self.send_message(chat_id, text[:-1])
+        
+        
+        
+        
+    def get_user_icon(self) -> str:
+        """Assigns a unique animal icon for each user."""
+        icon = random.choice(self.icons)
+        self.icons.remove(icon)
+        return icon
+
+    async def chat(self, chat_id, username, text) -> None:
+        icon = self.get_user_icon(username)
+        self.in_chat_users[username] = (chat_id, icon)
+        text = "You are in chat now! Currently in chat:\n\n"
+
+        for uname, (cid, icn) in self.in_chat_users.items():
+            text += f"@{uname} {icn}\n"
+            if cid != chat_id:
+                self.send_message(cid, parse_mode=None)
+
+        self.send_message(chat_id, parse_mode=None)
+        return 0
+
+    # async def exit(self, update, context) -> int:
+    #     """Exits and ends the conversation."""
+    #     username = update.message.from_user.username
+    #     chat_id = update.message.chat_id
+
+    #     self.send_message(chat_id, parse_mode=None)
+
+    #     if username in self.in_chat_users:
+    #         del self.in_chat_users[username]
+
+    #     for uname, (cid, icon) in self.in_chat_users.items():
+    #         if cid != chat_id:
+    #             self.send_message(cid, parse_mode=None)
+
+    #     return 0
+    
+    
+    def start(self, chat_id):
+        self.send_message(
+            chat_id,
+            "Welcome to smuhq bot! This bot is designed to better facilitate HQ!\n\n"
+            "Available Commands:\n\n"
+            "<strong>chat</strong>\n"
+            "/chat - join a virtual chat group\n"
+            "any message - broadcast message to those who are in chat\n"
+            "/in_chat - list all users in chat\n"
+            "/exit - exit chat group",
+            parse_mode="HTML"
+        )
--- lambda_function.py
+++ lambda_function.py
@@ -11,16 +11,7 @@ if os.environ.get('AWS_LAMBDA_FUNCTION_NAME') is None:
 bot_token = os.environ.get("BOT_TOKEN")
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{bot_token}"
 
-def send_message(chat_id, text, parse_mode=None):
-    url = f"{TELEGRAM_API_URL}/sendMessage"
 
-    payload = {
-        "chat_id": chat_id,
-        "text": text,
-    }
-    if parse_mode:
-        payload["parse_mode"] = parse_mode
-    requests.post(url, json=payload)
 
 
 
@@ -33,29 +24,29 @@ def lambda_handler(event, context):
     if body is None:
         return {"statusCode": 400, "body": "No body found"}
 
+
+    # message = {'message_id': 1706, 'from': {'id': 1272231156, 'is_bot': False, 'first_name': 'Jinming', 'last_name': 'Cao', 'username': 'repurika', 'language_code': 'zh-hans'}, 'chat': {'id': 1272231156, 'first_name': 'Jinming', 'last_name': 'Cao', 'username': 'repurika', 'type': 'private'}, 'date': 1756377079, 'text': '123'}
+
     update = json.loads(body)
     message = update.get("message", {})
     chat_id = message.get("chat", {}).get("id")
+    username = message.get("from", {}).get("username")
     text = message.get("text", "")
 
     if not chat_id:
         return {"statusCode": 200, "body": "No chat_id"}
 
     if text.startswith("/start"):
-        send_message(
-            chat_id,
-            "Welcome to smuhq bot! This bot is designed to better facilitate HQ!\n\n"
-            "Available Commands:\n\n"
-            "<strong>chat</strong>\n"
-            "/chat - join a virtual chat group\n"
-            "any message - broadcast message to those who are in chat\n"
-            "/in_chat - list all users in chat\n"
-            "/exit - exit chat group",
-            parse_mode="HTML"
-        )
-    else:
-        virtual_chat.add_message(message, text)
-        virtual_chat.send_message(chat_id)
+        virtual_chat.start(chat_id)
+    elif text.startswith("/chat"):
+        virtual_chat.chat(chat_id, username, text)
+    elif text.startswith("/in_chat"):
+        virtual_chat.in_chat(chat_id, text)
+    elif text.startswith("/exit"):
+        virtual_chat.exit(chat_id, text)
+    
+        # virtual_chat.add_message(message, text)
+        # virtual_chat.send_message(chat_id)
         # send_message(chat_id, {text})
         
 
