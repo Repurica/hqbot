864ffc8d228aec34f951119077b694546fd7a9f4
Repurica <65117070+Repurica@users.noreply.github.com>
2025-08-28 21:24:58 +0800

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -30,7 +30,7 @@ def lambda_handler(event, context):
     chat_id = message.get("chat", {}).get("id")
     username = message.get("from", {}).get("username")
     
-    # virtual_chat.send_message(chat_id, message)
+    virtual_chat.send_message(chat_id, message)
 
     if not chat_id:
         return {"statusCode": 200, "body": "No chat_id"}
