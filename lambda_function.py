3dd317d522490b51b4324b4ec9b4b97cc92ece50
Repurica <65117070+Repurica@users.noreply.github.com>
2025-08-28 19:27:29 +0800

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -24,13 +24,13 @@ def lambda_handler(event, context):
         return {"statusCode": 400, "body": "No body found"}
 
 
-    # message = {'message_id': 1706, 'from': {'id': 1272231156, 'is_bot': False, 'first_name': 'Jinming', 'last_name': 'Cao', 'username': 'repurika', 'language_code': 'zh-hans'}, 'chat': {'id': 1272231156, 'first_name': 'Jinming', 'last_name': 'Cao', 'username': 'repurika', 'type': 'private'}, 'date': 1756377079, 'text': '123'}
 
     update = json.loads(body)
     message = update.get("message", {})
     chat_id = message.get("chat", {}).get("id")
     username = message.get("from", {}).get("username")
     text = message.get("text", "")
+    virtual_chat.send_message(chat_id, {'message_id': 1706, 'from': {'id': 1272231156, 'is_bot': False, 'first_name': 'Jinming', 'last_name': 'Cao', 'username': 'repurika', 'language_code': 'zh-hans'}, 'chat': {'id': 1272231156, 'first_name': 'Jinming', 'last_name': 'Cao', 'username': 'repurika', 'type': 'private'}, 'date': 1756377079, 'text': '123'})
 
     if not chat_id:
         return {"statusCode": 200, "body": "No chat_id"}
